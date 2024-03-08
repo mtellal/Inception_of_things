ca5be26046ec35f7f1b7103b84e803782d7b8e4f
Mezyann TELLAL <mtellal@paul-f4Ar4s1.clusters.42paris.fr>
2024-03-08 14:39:49 +0100

    p2 - deploy all files in shared/

--- p2/shared/server.sh
+++ p2/shared/server.sh
@@ -6,8 +6,8 @@ SERVER_IP=192.168.56.110
 echo "INSTALLING K3S SERVER"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=$SERVER_IP --flannel-iface=$INTERFACE --write-kubeconfig-mode=644" sh - 
 
-echo "DEPLOYING K3S CONFIG FILE"
-sudo kubectl apply -f /home/vagrant/shared/deployment.yaml
+echo "DEPLOYING K3S CONFIG FILES"
+sudo kubectl apply -f /home/vagrant/shared/
 
 echo "ADDING K ALIAS ...."
 sudo -u vagrant bash -c 'echo "alias k='\''kubectl'\''" >> /home/vagrant/.bashrc'
